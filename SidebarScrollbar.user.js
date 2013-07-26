ab2353539d3eff1b6a9a58109c7020895ac17f21
HodofHod <a.mishulovin@gmail.com>
2013-07-26 10:20:18 -0500

    Update SidebarScrollbar.user.js

--- SidebarScrollbar.user.js
+++ SidebarScrollbar.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name         Sidebar Scrollbar
-// @description  Makes the lower starred messages in SE chat rooms accessible.
+// @name         Chat Fixes
+// @description  Fixes some bugs in StackExchange's chat rooms
 // @include      http://chat.meta.stackoverflow.com/rooms/*
 // @include      http://chat.stackexchange.com/rooms/*
 // @include      http://chat.stackoverflow.com/rooms/*
@@ -10,24 +10,33 @@
 // @match        http://chat.stackoverflow.com/rooms/*
 // @match        http://chat.askubuntu.com/rooms/*
 // @author       @HodofHod
-// @namespace    HodofHod
-// @version      1.1
+// @version      1.2
 // ==/UserScript==
 
 function inject(f) {
-    var script = document.createElement('script');
+  var script = document.createElement('script');
     script.type = 'text/javascript';
     script.textContent = '(' + f.toString() + ')()';
-    document.body.appendChild(script);
-}
+  document.body.appendChild(script);
+};
 
+//Makes the lower starred messages in SE chat rooms accessible.
 function addScroll() {
     $('#sidebar').css({
                         height:'auto',
-                        bottom:'88px',
+                        bottom:$('#input-area').outerHeight(),
                         'overflow-y':'auto',
                     });
     $('#my-rooms').css("margin-left", "18px")
 }
 
+//Fixes bidirectional bug in starred chat messages.
+function bidi(){
+    reg = /[\p{InHebrew}\p{InArabic}]/;
+	$.each($('[id^=summary_]'), function(i, star){
+		reg.test($(star).html()) && $(star).html($(star).html().replace(/ - <a/, ' &lrm;- <a'));
+	});
+}
+
 inject(addScroll);
+inject(bidi);
