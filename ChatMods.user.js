1c64ff5290548959669426d8105ed38ca9bcd718
HodofHod <a.mishulovin@gmail.com>
2013-08-01 22:48:48 -0500

    Added simple scrolling for replies in transcript pages.

--- ChatMods.user.js
+++ ChatMods.user.js
@@ -1,14 +1,10 @@
 // ==UserScript==
 // @name         Chat Mods
 // @description  Modifications and bug fixes for StackExchange's chat rooms
-// @include      http://chat.meta.stackoverflow.com/rooms/*
-// @include      http://chat.stackexchange.com/rooms/*
-// @include      http://chat.stackoverflow.com/rooms/*
-// @include      http://chat.askubuntu.com/rooms/*
-// @match        http://chat.meta.stackoverflow.com/rooms/*
-// @match        http://chat.stackexchange.com/rooms/*
-// @match        http://chat.stackoverflow.com/rooms/*
-// @match        http://chat.askubuntu.com/rooms/*
+// @match        http://chat.meta.stackoverflow.com/*
+// @match        http://chat.stackexchange.com/*
+// @match        http://chat.stackoverflow.com/*
+// @match        http://chat.askubuntu.com/*
 // @author       @HodofHod
 // @version      1.2
 // ==/UserScript==
@@ -23,9 +19,9 @@ function inject(f) {
 //Makes the lower starred messages in SE chat rooms accessible.
 function addScroll() {
     $('#sidebar').css({
-                        height:'auto',
-                        bottom:$('#input-area').outerHeight(),
-                        'overflow-y':'auto',
+                    height:'auto',
+                    bottom:$('#input-area').outerHeight(),
+                    'overflow-y':'auto',
                     });
     $('#my-rooms').css("margin-left", "18px")
 }
@@ -33,10 +29,27 @@ function addScroll() {
 //Fixes bidirectional bug in starred chat messages.
 function bidi(){
     reg = /[\p{InHebrew}\p{InArabic}]/;
-	$.each($('[id^=summary_]'), function(i, star){
-		reg.test($(star).html()) && $(star).html($(star).html().replace(/ - <a/, ' &lrm;- <a'));
-	});
+    $.each($('[id^=summary_]'), function(i, star){
+        reg.test($(star).html()) && $(star).html($(star).html().replace(/ - <a/, ' &lrm;- <a'));
+    });
 }
 
-inject(addScroll);
-inject(bidi);
+function followReply(){
+    $('.reply-info').click(function(){
+        anchor = this.href.match(/#\d+$/)[0];
+        if ($('#message-' + anchor.slice(1)).length){
+            this.href = anchor;
+            $('.highlight').removeClass('highlight');
+            var msg = $('#message-' + anchor.slice(1));
+            msg.addClass('highlight');
+            $('html, body').animate({ scrollTop: msg.offset().top }, 'fast')
+        }
+    });
+}
+
+if (/^\/rooms\//.test(window.location.pathname)){
+    inject(addScroll);
+    inject(bidi);
+}else if (/^\/transcript\//.test(window.location.pathname)){
+    inject(followReply);
+}
