4ca3d84b505ca118779b38948c09cca2cbcb239b
HodofHod <a.mishulovin@gmail.com>
2013-10-23 22:31:48 -0500

    Versioning.

--- SE-HebrewKeyboard.user.js
+++ SE-HebrewKeyboard.user.js
@@ -10,7 +10,7 @@
 // @match         http://chat.stackexchange.com/rooms/*
 // @author        HodofHod
 // @namespace     HodofHod
-// @version       0.4.1
+// @version       0.4.2
 // ==/UserScript==
 
 
